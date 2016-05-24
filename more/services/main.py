d80b8319426672576b4a66ba434e0736120082c0
Blaise Laflamme <blaise@laflamme.org>
2016-05-23 20:48:35 -0400

    registry depends on settings

--- more/services/main.py
+++ more/services/main.py
@@ -1,5 +1,7 @@
 from dectate import Action
 from morepath import App
+from morepath.directive import SettingAction
+from morepath.settings import SettingRegistry
 from reg import (
     Registry,
     match_argname
@@ -18,12 +20,33 @@ class ServiceApp(App):
 
 class ServiceRegistry(Registry):
 
+    factory_arguments = {
+        'setting_registry': SettingRegistry
+        }
+
+    def __init__(self, setting_registry):
+        self._setting_registry = setting_registry
+        super().__init__()
+
+    def register_service(self, obj, name):
+        self.register_predicates(
+            name,
+            [match_argname('obj')]
+        )
+        self.register_value(
+            name,
+            [Service],
+            obj(self, self._setting_registry)
+        )
+
     def find(self, name):
         return self.component(name, Service)
 
 
 @ServiceApp.directive('service')
 class ServiceAction(Action):
+    depends = [SettingAction]
+
     config = {
         'service_registry': ServiceRegistry
     }
@@ -35,12 +58,4 @@ class ServiceAction(Action):
         return ('service', self.name)
 
     def perform(self, obj, service_registry):
-        service_registry.register_predicates(
-            self.name,
-            [match_argname('obj')]
-        )
-        service_registry.register_value(
-            self.name,
-            [Service],
-            obj(service_registry)
-        )
+        service_registry.register_service(obj, self.name)
